89bf05386efad7e6e126dab3a3ca9f00cbeed334
maltiraifi <maltiraifi22@gmail.com>
2025-08-04 02:28:24 +0300

    Add handling multiple files feature.

--- rmv.c
+++ rmv.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Check for number of arguments 
-  // No more than 2 for the first iteration
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <file>\n", argv[0]);
+  // if +1 files failed, set status to 1 to indicate failure.
+  int exit_status = EXIT_SUCCESS;
+
+  // 1. Check at least 2 arguments are present
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s <file1> [<file2>, ...]\n", argv[0]);
     return EXIT_FAILURE;
   }
  
-  // file removal attempt
-  if (remove(argv[1]) != 0) {
-    perror(argv[1]);
-    return EXIT_FAILURE;
-  } 
-
-  // success message
-  printf("File %s successfully removed", argv[1]);
-  
-  // successful execution
-  return EXIT_SUCCESS;
+  // 2. Loop through argc, remove files, and handle errors 
+  for (int i = 1; i < argc; i++) {
+    const char *file_name = argv[i];
+    if (remove(file_name) != 0) {
+      perror(file_name);
+      exit_status = 1;
+    } else {
+      printf("File '%s' successfully removed.\n", file_name);
+    }
+  }
+ 
+  // 3. Return success status
+  return exit_status;
 }
