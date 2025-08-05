6f1d302d7211105c336ff04c7114d46e9d392be8
maltiraifi <maltiraifi22@gmail.com>
2025-08-05 19:57:22 +0300

    Add interactive prompting

--- trashman.c
+++ trashman.c
@@ -1,21 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h> // for getopt_long()
+#include <ctype.h> // for tolower()
+#include <stdbool.h>
+#include <string.h> // strcspn(), strchr()
+
+void to_lower(char *str) {
+  for (int i = 0; str[i]; i++) {
+    str[i] = tolower(str[i]);
+  }
+}
+
+bool prompt_ans(const char *file_name) {
+  // Assign a buffer for user input
+  char buf[8];
+
+  // while loop; re-prompting till valid
+  while (1) {
+    printf("trashman: remove file '%s'? ", file_name); 
+    fflush(stdout);
+
+    if (fgets(buf, sizeof(buf), stdin) == NULL) {
+      printf("\n");
+      return false;
+    }
+
+    // Handle potential buffer overflow
+    if (strchr(buf, '\n') == NULL) {
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF);
+    }
+
+    buf[strcspn(buf, "\n")] = '\0';
+    to_lower(buf);
+
+    if (buf[0] == 'y') return true;
+    if (buf[0] == 'n') return false;
+    printf("Invalid input. Please enter 'y' or 'n'.\n");
+ }
+}
 
 int main(int argc, char *argv[]) {
   int exit_status = EXIT_SUCCESS;
   int verbose = 0; // Verbose logic indicator 
+  int interactive = 0;
   
   struct option long_opts[] = {
     {"verbose", no_argument, 0, 'v'},
+    {"interactive", no_argument, 0, 'i'},
     {0, 0, 0, 0}
   };
 
   int opt;
-  while ((opt = getopt_long(argc, argv, "v", long_opts, NULL)) != -1) {
+  while ((opt = getopt_long(argc, argv, "vi", long_opts, NULL)) != -1) {
     switch (opt) {
       case 'v':
         verbose = 1;
+        break;
+      case 'i': 
+        interactive = 1;
         break;
       case '?':
         if (optopt)
@@ -29,12 +72,17 @@ int main(int argc, char *argv[]) {
 
   if (optind >= argc) {
     fprintf(stderr, "Error: No files specified.\n");
-    fprintf(stderr, "usage: %s [-v|--verbose] <file1> [<file2> ...]\n", argv[0]);
+    fprintf(stderr, "usage: %s [-v|--verbose] [-i|--interactive] <file1> [<file2> ...]\n", argv[0]);
     return EXIT_FAILURE;
   }
  
   for (int i = optind; i < argc; i++) {
     const char *file_name = argv[i];
+
+    if (interactive && !prompt_ans(file_name)) {
+      continue; // skip current iteration
+    }
+
     if (remove(file_name) != 0) {
       perror(file_name);
       exit_status = EXIT_FAILURE;
